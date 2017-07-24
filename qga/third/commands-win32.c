90ec22f1224e4c1e229113db21d9ced3fc7021e1
zhaozhongguo <zhao_zhong_guo@sina.com>
2017-07-24 09:56:27 +0800

    add new third dir

---
+++ qga/third/commands-win32.c
@@ -0,0 +1,36 @@
+/*
+ * QEMU Guest Agent win32-specific command implementations
+ *
+ * Copyright IBM Corp. 2012
+ *
+ * Authors:
+ *  Michael Roth      <mdroth@linux.vnet.ibm.com>
+ *  Gal Hammer        <ghammer@redhat.com>
+ *
+ * This work is licensed under the terms of the GNU GPL, version 2 or later.
+ * See the COPYING file in the top-level directory.
+ */
+
+#include "qemu/osdep.h"
+#include <wtypes.h>
+#include <powrprof.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <iptypes.h>
+#include <iphlpapi.h>
+#ifdef CONFIG_QGA_NTDDSCSI
+#include <winioctl.h>
+#include <ntddscsi.h>
+#include <setupapi.h>
+#include <initguid.h>
+#endif
+#include <lm.h>
+
+#include "qga/guest-agent-core.h"
+#include "qga/vss-win32.h"
+#include "qga-qmp-commands.h"
+#include "qapi/qmp/qerror.h"
+#include "qemu/queue.h"
+#include "qemu/host-utils.h"
+#include "qemu/base64.h"
+
